7ebf22732fbbc15a4336227736f894f429d5b64a
K3-lvin <k.rafaldine.69@gmail.com>
2025-07-29 14:58:29 -0300

    29/07

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -1,9 +1,40 @@
-export default function App()
-{
+import "./App.css";
+
+export default function App() {
     return (
         <div>
             <h1>Estudo CSS</h1>
-            
+
+            <p>
+                O CSS <i> Cacasding Style Sheets </i> é uma limguagem utilozada para estilizar <b>página web</b>, e será ultiaxada dentro ou fora do HTML.
+            </p>
+
+            <p>
+                Na aula de hoje, vamos estudar e entender como aplicar estilo css em conponentes do React.
+            </p>
+            <p>Ultiizando o css podemos defiir:</p>
+
+            <ul>
+                <li>Definir cor para o texto, para o fundo do elemento</li>
+                <li>Fomtes de tamanho e letras</li>
+                <li>posicionamentos de elementos na pagina</li>
+                <li>Espaçamento, bordas, sombras etc...</li>
+                <li>Efeitos visuais e animação</li>
+            </ul>
+
+            <p>
+                O HTML diz o que deve aparecer na pagina, enquanto o CSSdiz como deve aparecer.
+            </p>
+
+            <p className="centro">
+                Vee - The Owl House
+            </p>
+
+            <p className="centro2">
+                <img src="_image.webp" alt="" />
+            </p>
+
+            <h3> FIM DA AULA!!! </h3>
         </div>
     );
 }
