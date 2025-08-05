8182db3f63899f58a045a8558e821eafb7ad1071
K3-lvin <k.rafaldine.69@gmail.com>
2025-08-05 14:52:35 -0300

    05-08

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,21 @@
+export default function SaoPaulo ()
+{
+    return (
+        <div>
+            <h1>Seja bem vindo ao estdo de São Paulo</h1>
+
+<p>
+    O estado de São Paulo é o mais populoso e economicamente desenvolvido do Brasil. É um grande centro industrial, financeiro e cultural, com a capital homônima sendo uma das maiores metrópoles do mundo. Além disso, possui diversidade geográfica, incluindo áreas urbanas, rurais e reservas naturais.
+</p>
+ 
+<p>
+    <img src="" />
+</p>
+
+<p>
+    <a href="/"> Voltar para Home </a>
+</p>
+            
+        </div>
+    )
+}
