6df765bb225bc8ef6f7253c1bf7ade8f80723842
K3-lvin <k.rafaldine.69@gmail.com>
2025-09-02 14:53:37 -0300

    02/09

---
+++ estudoestados/src/Paginas/Exemplo1.jsx
@@ -0,0 +1,32 @@
+export default function Exemplo1() {
+  return (
+    <div>
+      <h1>Exemplo 1</h1>
+
+      <div className="conteudo">
+
+<h3>Exemplo do useStete</h3>
+
+<p>O objetivo aqui será receber dois números, soma-lós, e exibir o resultado</p>
+
+<form>
+  <p>
+    Digite o Primeiro número<br />
+    <input type="text" />
+  </p>
+
+<p>
+digite o segundo número <br />
+<input type="text" />
+</p>
+
+<p>
+  <input type="button" value="Caucular" />
+</p>
+</form>
+
+      </div>
+
+    </div>
+  );
+}
---
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -0,0 +1,40 @@
+export default function Exemplo2() {
+  return (
+    <div>
+      <h1>Exemplo 2</h1>
+
+
+      <div className="conteudo">
+
+<h3>Exemplo do useStete</h3>
+
+<p>Receba o nome a as dua notas do aluno, logo depois calcule a média</p>
+
+<form>
+  <p>
+    Nome completo<br />
+    <input type="text" />
+  </p>
+
+<p>
+ Primeira nota <br />
+<input type="text" />
+</p>
+
+
+<p>
+Segunda nota <br />
+<input type="text" />
+</p>
+
+<p>
+  <input type="button" value="Caucular" />
+</p>
+</form>
+
+      </div>
+
+      
+    </div>
+  );
+}
